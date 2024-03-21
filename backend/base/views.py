7c4c5f22d3d5f2e5e4754262e25c104d62e20787
musamauet <usamarabani@renesistech.com>
2024-03-21 12:43:24 +0500

    implement base service

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    routes = ['/api/products','/api/products/<id>', '/api/products/create', '/api/products/all']
+    return Response(routes)
