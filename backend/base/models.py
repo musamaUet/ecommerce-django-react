a3a9a78f956c835beef90ece54d501d6994ac74e
musamauet <usamarabani@renesistech.com>
2024-03-21 22:03:14 +0500

    update models impleemntation and add serialization

--- backend/base/models.py
+++ backend/base/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete = models.SET_NULL, null= True)
     name = models.CharField(max_length = 200, null = True, blank = True)
-    # image = ''
+    image = models.ImageField(blank=True, null=True)
     brand = models.CharField(max_length = 200, null = True, blank = True)
     category = models.CharField(max_length = 200, null= True, blank = True)
     description = models.TextField(null = True, blank = True)
@@ -54,7 +54,7 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, null = True, blank = True)
     qty = models.IntegerField(null = True, blank = True, default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2, null = True, blank=True)
-    image = models.CharField(max_length=200, blank = True, null = True)
+    image = models.ImageField(blank = True, null = True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
