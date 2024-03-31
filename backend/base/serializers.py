7c32ebab50d463f9029e9a931ebe17cab95c2199
musamauet <usamarabani@renesistech.com>
2024-03-31 17:45:23 +0500

    implement signup user api

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import mask_hash
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
